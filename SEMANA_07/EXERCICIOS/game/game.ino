72e454da09952a00fa060377bc9f6a8eb67e6822
Eric_Tach <eric.tachdjian@sou.inteli.edu.br>
2022-12-08 14:27:19 -0300

    ExSm 7

--- SEMANA_07/EXERCICIOS/game/game.ino
+++ SEMANA_07/EXERCICIOS/game/game.ino
@@ -66,15 +66,30 @@ void loop(){
             client.println();
 
             // Mostrar a pagina HTML em si
-            client.println("<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>Cara ou coroa!</title></head><body><div id='main'><h1>Cara ou coroa!?</h1><button onclick='caracoroa()'>Girar a moeda</button><p id='moed'></p><img id='caramoeda' src='https://i.pinimg.com/originals/75/90/c0/7590c002386d1107c5b5c5b0b333994a.png' alt='caramoeda' style='display: none;' ><img id='coroamoeda' src='https://i.pinimg.com/originals/66/13/d2/6613d23bff61c8cb0c03885415bd1e9d.png' alt='coroamoeda' style='display: none;'></div><script>function caracoroa() {var moeda = Math.floor(Math.random() * 2);if (moeda == 0) {document.getElementById('moed').innerHTML = 'Cara';document.getElementById('caramoeda').style.display = 'block';document.getElementById('coroamoeda').style.display = 'none';} else {document.getElementById('moed').innerHTML = 'Coroa';document.getElementById('coroamoeda').style.display = 'block';document.getElementById('caramoeda').style.display = 'none'; }}</script><style>#coroamoeda{width: 350px;height: 350px;margin-left: -20px;margin-top: -20px;}#main{position: absolute;top: 3%;left: 40%;width: 350px;height: 350px;}</style></body></html>");
-            
+               client.println(content='width=device-width, initial-scale=1.0'><title>Cara ou coroa!</title></head>")
+              client.println("<body><div id='main'><h1>Cara ou coroa!?</h1><button onclick='caracoroa()'>Girar a moeda</button><p id='moed'></p><img id='caramoeda' src='https://i.pinimg.com/originals/75/90/c0/7590c002386d1107c5b5c5b0b333994a.png' alt='caramoeda' style='display: none;' ><img id='coroamoeda' src='https://i.pinimg.com/originals/66/13/d2/6613d23bff61c8cb0c03885415bd1e9d.png' alt='coroamoeda' style='display: none;'></div><script>function caracoroa() {var moeda = Math.floor(Math.random() * 2);if (moeda == 0) {document.getElementById('moed').innerHTML = 'Cara';document.getElementById('caramoeda').style.display = 'block';document.getElementById('coroamoeda').style.display = 'none';} else {document.getElementById('moed').innerHTML = 'Coroa';document.getElementById('coroamoeda').style.display = 'block';document.getElementById('caramoeda').style.display = 'none'; }}</script><style>#coroamoeda{width: 350px;height: 350px;margin-left: -20px;margin-top: -20px;}#main{position: absolute;top: 3%;left: 40%;width: 350px;height: 350px;}</style></body></html>");
 
             // a resposta HTTP termina com outra linha em branco:
             client.println();
             // saia do loop while:
             break;
+             if (currentLine.endsWith("GET /S")) {
+          long interval = random(1000, 10000); 
+          unsigned long previousMillis = millis(); 
+          unsigned long currentMillis = millis(); 
+          
+          digitalWrite(LED_CARA, LOW);
+          digitalWrite(LED_COROA, LOW);
+
+           if (digitalRead(LED_COROA) == LOW) {
+                digitalWrite(LED_CARA, HIGH); 
+          if (digitalRead(LED_CARA) == LOW) {
+              digitalWrite(LED_COROA, HIGH);                 
+              
 
     // feche a conexão:
     client.stop();
     Serial.println("Client Disconnected.");
     }
+           }
+             }           
